a07ebd27b48e8457e0fddd320b1258efd5309015
beenyamin <beenyaminkhan@gmail.com>
2023-09-26 11:33:18 +0600

    Added Item from detaile page to Donation route

--- src/pages/Donate/Donate.jsx
+++ src/pages/Donate/Donate.jsx
@@ -1,51 +1,70 @@
-import  { useEffect, useState } from 'react';
-import MultipleCard from '../Card/MultipleCard';
-import HomeCard from '../../Components/AllCard/HomeCard';
+import { useEffect, useState } from 'react';
 import DonateCard from './DonateCard';
 
 const Donate = () => {
 
-    const [donateItem, setDonateItem] = useState ([])
+  const [donateItem, setDonateItem] = useState([])
 
-    const [noDataFound, setNoDAtaFound] = useState ("")
+  const [noDataFound, setNoDAtaFound] = useState("")
 
-    useEffect( () => {
+  const [seeAll, setSeeAll] = useState (false)
 
-       const donationPage = JSON.parse(localStorage.getItem('donation'));
+  useEffect(() => {
 
-     if (donationPage) {
+    const donationPage = JSON.parse(localStorage.getItem('donation'));
 
-        setDonateItem(donationPage)
-        
-     } else
-     {
-        setNoDAtaFound ('No Donation Here')
+    if (donationPage) {
 
-     }
+      setDonateItem(donationPage)
+
+    } else {
+      setNoDAtaFound('No Donation Here')
+
+    }
 
 
-    }, [])
-   
-console.log(donateItem)
+  }, [])
 
-    return (
-        <div>
+  console.log(donateItem)
+
+  console.log(seeAll)
+
+  return (
+    <div>
+
+
+
+      {
+
+        noDataFound ? <p className=' h-64 font-bold text-2xl flex justify-center items-center'>{noDataFound}</p> :
+          <div>
+
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 mx-auto w-10/12'>
+
+              { seeAll? 
+              
+              donateItem.map(card => <DonateCard key={card.id} card={card}></DonateCard>) :
+              
+              donateItem.slice(0,4).map(card => <DonateCard key={card.id} card={card}></DonateCard>) }
+
+
+            </div>
           
-          { 
 
-         noDataFound ? <p className=' h-64 font-bold text-2xl flex justify-center items-center'>{ noDataFound}</p> :
-         <div> 
+         <div className='text-center'>
 
-   <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 mx-auto w-7/12'>
 
-    { donateItem.map ( card => <DonateCard key={card.id}  card={card}></DonateCard> ) }
-   </div>
+         { donateItem.length > 4 &&  <button onClick={() => setSeeAll(!seeAll)}   className='px-5 py-1 text-[#FFF]  rounded mx-auto text-lg font-semibold bg-[#009444]'> {seeAll? "See Less" : "See all"}</button>}
 
          </div>
-          
-          }
-        </div>
-    );
+
+
+          </div>
+
+      }
+
+    </div>
+  );
 };
 
 export default Donate;
